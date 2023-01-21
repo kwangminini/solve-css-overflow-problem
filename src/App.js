fd0889f9649b07c5cc69ca3288555746ca31cf22
kwangmin kim <rhkdals7362@gmail.com>
2023-01-21 14:37:36 +0000

    fix: overflow-y and overflow-x css problem to write tistory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import "./styles.css";
+import Tooltip from "./Tooltip";
 
 export default function App() {
+  const menuArr = new Array(10).fill(0);
+  const [openTooltip, setOpenTooltip] = useState(undefined);
+
+  const toggleTooltip = (idx, type) => {
+    setOpenTooltip(type === "enter" ? idx : undefined);
+  };
   return (
     <div className="App">
-      <section className="sideBar">
-        <div className="menu">메뉴</div>
+      <nav>네비</nav>
+      <section className="content">
+        <div
+          className={`sideBarWrapper ${openTooltip ? "tooltip" : undefined}`}
+        >
+          <div className="sideBar">
+            {menuArr.map((menu, idx) => {
+              return (
+                <div
+                  className="menu"
+                  key={idx}
+                  onMouseEnter={() => toggleTooltip(idx + 1, "enter")}
+                  onMouseLeave={() => toggleTooltip(idx + 1, "leave")}
+                >
+                  메뉴
+                  {openTooltip === idx + 1 && <Tooltip />}
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </section>
     </div>
   );
---
+++ src/Tooltip/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function Tooltip() {
+  return (
+    <div className="wrapper">
+      <p>이것은 툴팁 입니다.</p>
+    </div>
+  );
+}
