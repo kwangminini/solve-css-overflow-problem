384be754f21eaef58e948e36c201b2a8d7c033ea
kwangmin kim <48472989+kwangminini@users.noreply.github.com>
2023-01-21 18:41:10 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <section className="sideBar">
+        <div className="menu">메뉴</div>
+      </section>
+    </div>
+  );
+}
